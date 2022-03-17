e0b21280c290fc42cf79964dbb1b3fbf73b059da
root <admasterefe00@gmail.com>
2022-03-17 07:55:22 -0700

    more on nested loops and functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+/**
+ * do fizz buzz
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if(i % 15 == 0)
+		{
+			printf("%s", "FizzBuzz ");
+		}
+		else if(i % 3 == 0)
+		{
+			printf("%s", "Fizz ");
+		}
+		else if(i % 5 == 0)
+		{
+		       printf("%s", "Buzz ");	
+		}
+		else
+		{
+			printf("%d" "%s", i, " ");
+		}
+		
+	}
+	printf("\n");
+	return (0);
+}
